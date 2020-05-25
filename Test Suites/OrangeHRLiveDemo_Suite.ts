d7302fb7894a570ccd13e63e5399f61a805a16d2
ambavatdilip <43708116+ambavatdilip@users.noreply.github.com>
2020-05-25 23:53:35 +0530

    OrangeHRDemLive TestSuite Execution Report

---
+++ Test Suites/OrangeHRLiveDemo_Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>OrangeHRLiveDemo_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>cbf0551a-4bd9-409a-8efb-6f84607271ab</testSuiteGuid>
+   <testCaseLink>
+      <guid>45a2532c-b11b-4c2b-9ffe-288b634a9803</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OrangeHRMSLogin_TC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
